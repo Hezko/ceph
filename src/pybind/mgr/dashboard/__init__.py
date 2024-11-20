22573acf7a0eb22449f2b85a9e5dc60f0f271e06
Tomer Haskalovitch <il033030@Tomers-MBP.lan>
2024-11-20 09:51:08 +0200

    fix

--- src/pybind/mgr/dashboard/__init__.py
+++ src/pybind/mgr/dashboard/__init__.py
@@ -35,7 +35,7 @@ else:
     import logging
     logging.basicConfig(level=logging.DEBUG)
     logging.root.handlers[0].setLevel(logging.DEBUG)
-    fh = logging.FileHandler('/data/code/ceph-ci/build/tomerlog')
+    fh = logging.FileHandler('/data/code/ceph-ci/build/tomerlog/log.txt')
     fh.setLevel(logging.DEBUG)
     logging.root.addHandler(fh)
     import sys
