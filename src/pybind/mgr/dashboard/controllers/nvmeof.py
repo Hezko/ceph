f9eca0154ac3aa79d3b05c9edeb6a1e7dfc14bb4
Tomer Haskalovitch <il033030@Tomers-MBP.lan>
2024-11-27 15:47:39 +0200

    try again

--- src/pybind/mgr/dashboard/controllers/nvmeof.py
+++ src/pybind/mgr/dashboard/controllers/nvmeof.py
@@ -46,12 +46,23 @@ else:
         @map_model(model.GatewayInfo)
         @handle_nvmeof_error
         def list(self, gw_group: Optional[str] = None):
-            return NVMeoFGatewayClient.list(gw_group)
+            return NVMeoFClient(gw_group=gw_group).stub.get_gateway_info(
+                NVMeoFClient.pb2.get_gateway_info_req()
+            )
 
         @ReadPermission
         @Endpoint('GET')
         def group(self):
-            return NVMeoFGatewayClient.group()
+            try:
+                orch = OrchClient.instance()
+                return orch.services.list(service_type='nvmeof')
+            except OrchestratorError as e:
+                # just return none instead of raising an exception
+                # since we need this to work regardless of the status
+                # of orchestrator in UI
+                logger.error('Failed to fetch the gateway groups: %s', e)
+                return None
+
 
     @APIRouter("/nvmeof/subsystem", Scope.NVME_OF)
     @APIDoc("NVMe-oF Subsystem Management API", "NVMe-oF Subsystem")
