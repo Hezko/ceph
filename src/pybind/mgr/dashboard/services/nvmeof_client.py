ed784eef9813daacf013020907eff9560d8e544a
Tomer Haskalovitch <il033030@Tomers-MBP.lan>
2025-04-10 11:54:51 +0300

    mgr/dashboard: fix empty value error in pick decorator

--- src/pybind/mgr/dashboard/services/nvmeof_client.py
+++ src/pybind/mgr/dashboard/services/nvmeof_client.py
@@ -287,7 +287,7 @@ else:
                 model = func(*args, **kwargs)
                 field_to_ret = model[field]
                 if first:
-                    field_to_ret = field_to_ret[0]
+                    field_to_ret = field_to_ret[0] if field_to_ret else None
                 return field_to_ret
             return wrapper
         return decorator
